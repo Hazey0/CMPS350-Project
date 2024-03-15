f6e807c8e99a79a059ac0e6e0226fb8345026b84
Hazey0 <egle.man66@gmail.com>
2024-03-16 01:48:57 +0300

    search function V1
    i implemented a primative search function that searches the phones based on the keywords submitted by the search bar from the user. the only issue is that the name of the phone is still difficult to find like "galaxy" because the name of the phone can be of two words, same goes for the brand name such as "One Plus". will have to look further into an efficient solution to this problem.

--- Phase 1/Scripts/MyScript.js
+++ Phase 1/Scripts/MyScript.js
@@ -13,3 +13,41 @@ const checkUser= (use,pass) =>( users.reduce((a,v)=>
 use===v.username && pass===v.password ? v : a
 ))
 console.log(checkUser("ksjdbc",8793));
+class Phone{
+    brand;
+    name;
+    year;
+    price;
+    storage;
+    constructor(b,n,y,p,s){
+        brand=b;
+        name=n;
+        year=y;
+        price=p;
+        storage=s; 
+    }
+}
+const phones= [
+    {brand:"Samsung",name:"Galaxy S23",year:2023,price:1250,storage:256},
+    {brand:"Apple",name:"IPhone 14",year:2023,price:1300,storage:128},
+    {brand:"One Plus",name:"9T",year:2022,price:800,storage:256},
+    {brand:"Google",name:"Pixle 5",year:2018,price:650,storage:128},
+    {brand:"Samsung",name:"Galaxy S24 Ultra",year:2024,price:1300,storage:512},
+    {brand:"Nokia",name:"P10",year:2022,price:500,storage:64},
+    {brand:"Xaomi",name:"Mi 10",year:2023,price:1050,storage:256},
+    {brand:"Samsung",name:"Galaxy S21 Plus",year:2022,price:1000,storage:256},
+]
+const nameSplit=(a)=> a.split(" ");
+const searchQuery= "galaxy";
+const keywords= searchQuery.split(" ");
+const searchResult=phones.filter((e)=>
+e.brand.toLowerCase()==keywords.reduce((a,v)=> v.toLowerCase()==e.brand.toLowerCase() ? v:a) || 
+e.name.toLowerCase().split(" ").includes(keywords.map((d)=> d.toLowerCase()))||
+e.year==keywords.reduce((a,v)=> v==e.year ? v:a) ||
+e.storage==keywords.reduce((a,v)=> v==e.storage ? v:a)
+);
+searchResult2= phones.filter((e)=> e.brand.toLowerCase()==keywords.reduce((a,z)=> z.toLowerCase()==e.brand.toLowerCase() ? z.toLowerCase() : a.toLowerCase()))
+console.log(searchResult);
+const l=["kd","ew","fed","ed"];
+const e=l.includes("ew");
+console.log(e);
