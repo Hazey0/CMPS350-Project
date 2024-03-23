f66b30b11c1553e9d9671983d65d0ffbd898d735
QUHNUKK <99412376+QUHNUKK@users.noreply.github.com>
2024-03-23 21:03:22 +0300

    Fixed extra login page tabs

--- Phase 1/Scripts/loginInit.js
+++ Phase 1/Scripts/loginInit.js
@@ -1,33 +1,41 @@
-document.addEventListener("DOMContentLoaded",()=>{
-    const usersData=localStorage.getItem("users");
-    const users=JSON.parse(usersData);
+document.addEventListener("DOMContentLoaded", () => {
+    const usersData = localStorage.getItem("users");
+    const users = JSON.parse(usersData);
     console.log(users);
 
-    function log(){
-        const loginButton=document.querySelector("#loginButton");
-        loginButton.addEventListener("click",(event)=>{
-            const pass=document.querySelector("#password").value;
-            const username=document.querySelector("#username").value;
-            if(pass !="" && username !=""){
-            console.log(username,pass);
-            let tempUser=checkUser(username,pass);
-            //if(tempUser.length==0){
-            //    tempUser=null;
-            //}
-            //else{
-            //    tempUser=tempUser.pop();
-            //}
-            console.log(tempUser);
-            if(tempUser!=null){
-                localStorage.setItem("user",JSON.stringify(tempUser));
-                open("./main.html");
-            }}
-        })
+    function log() {
+        const loginButton = document.querySelector("#loginButton");
+        loginButton.addEventListener("click", (event) => {
+            const pass = document.querySelector("#password").value;
+            const username = document.querySelector("#username").value;
+            if (pass !== "" && username !== "") {
+                console.log(username, pass);
+                let tempUser = checkUser(username, pass);
+                console.log(tempUser);
+                // If tempUser is an empty array, no user was found
+                if (tempUser.length > 0) {
+                    localStorage.setItem("user", JSON.stringify(tempUser[0]));
+                    open("./main.html");
+                    close("./login.html") //apply on main.html part
+                } else {
+                    // Alert the user if login is incorrect
+                    alert("Incorrect username or password. Please try again.");
+                    open("./login.html");
+                    close("./login.html") 
 
+                }
+            }else{
+                    // Alert the user if login is incorrect
+                    alert("No username or password. Please try again.");
+                    open("./login.html");
+                    close("./login.html") 
 
+                
+            }
+        });
     }
 
-    const checkUser= (use,pass) =>( users.filter((v)=> v.password==pass && v.username==use ).reduce((a,b)=> b==null ? a : b)            );
+    const checkUser = (use, pass) => users.filter((v) => v.password == pass && v.username == use);
 
-    log()
+    log();
 });
