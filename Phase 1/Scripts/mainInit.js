2b2202f828dbfb41af87b5a385484ff23d081cfe
ahmed-ak2005685 <143374915+ahmedfaseeh-aa2005685@users.noreply.github.com>
2024-03-27 14:04:12 +0300

    Hid the buttons again, improved their icons as well

--- Phase 1/Scripts/mainInit.js
+++ Phase 1/Scripts/mainInit.js
@@ -603,19 +603,22 @@ document.addEventListener("DOMContentLoaded", () => {
 
     function showUserTab() {
         if (user !== null) {
-            const userContainer = document.querySelector("#userInfo");
-            //closeUserTab();
+            const userContainer = document.querySelector("#user");
+            closeUserTab();
             const userTab = document.createElement("div");
             userTab.classList.add("userTab");
 
             if (user.type == "Customer") {
 
+                const trans = document.createElement("div");
+                trans.classList.add("transDiv");
+                
                 const transaction = document.createElement("p");
                 const transactionLogo = document.createElement("img");
-
+                
 
                 transaction.classList.add("transaction");
-                transactionLogo.classList.add("sellLogo");
+                transactionLogo.classList.add("transLogo");
 
                 userTab.style.cursor = "pointer";
                 userTab.addEventListener("click", (event) => {
@@ -626,64 +629,94 @@ document.addEventListener("DOMContentLoaded", () => {
                 transaction.addEventListener("click", (event) => {
                     window.open("./transactions.html", "_self");
                 })
+
                 transactionLogo.style.cursor = "pointer";
                 transactionLogo.addEventListener("click", (event) => {
                     window.open("./transactions.html", "_self");
                 })
                 transactionLogo.src = "../Media/Icons/icons8-letter-64.png";
-                transaction.innerHTML = "Transaction History";
+                transaction.innerHTML = "Transactions";
 
-                userTab.appendChild(transaction);
-                userTab.appendChild(transactionLogo);
+                trans.appendChild(transaction);
+                trans.appendChild(transactionLogo);
+                userTab.appendChild(trans);
 
-                userContainer.appendChild(userTab)
-                // userContainer.addEventListener("click", (event) => {
-                //     const u = document.querySelector(".username")
-                //     const ui = document.querySelector(".userImage")
-                //     u.style.display = "none";
-                //     userContainer.appendChild(userTab);
-                // })
+                userContainer.addEventListener("click", (event) => {
+                    const u = document.querySelector(".username")
+                    const ui = document.querySelector(".userImage")
+                    u.style.display = "none";
+                    userContainer.appendChild(userTab);
+                })
 
             }
 
             else if (user.type == "Seller") {
+                const sellDiv = document.createElement("div");
+                sellDiv.classList.add("sellDiv");
+                const saleDiv = document.createElement("div");
+                saleDiv.classList.add("saleDiv");
+
                 const sell = document.createElement("p");
                 const sellLogo = document.createElement("img");
-
                 sell.classList.add("sell");
                 sellLogo.classList.add("sellLogo");
 
-                userTab.style.cursor = "pointer";
-                userTab.addEventListener("click", (event) => {
-                    window.open("./sell.html", "_self");
-                })
+                const sale = document.createElement("p");
+                const saleLogo = document.createElement("img");
+                sale.classList.add("sale");
+                saleLogo.classList.add("saleLogo");
 
                 sell.style.cursor = "pointer";
                 sellLogo.style.cursor = "pointer";
+                sale.style.cursor = "pointer";
+                saleLogo.style.cursor = "pointer";
+
                 sell.addEventListener("click", (event) => {
                     window.open("./sell.html", "_self");
                 })
                 sellLogo.addEventListener("click", (event) => {
                     window.open("./sell.html", "_self");
                 })
-                sellLogo.src = "../Media/Icons/icons8-letter-64.png";
-                sell.innerHTML = "Sell Item";
-                userTab.appendChild(sell);
-                userTab.appendChild(sellLogo);
+                sellDiv.addEventListener("click", () =>{
+                    window.open("./sell.html", "_self");
+                })
 
-                userContainer.appendChild(userTab)
-                // userContainer.addEventListener("click", (event) => {
-                //     const u = document.querySelector(".username")
-                //     const ui = document.querySelector(".userImage")
-                //     u.style.display = "none";
-                //     userContainer.appendChild(userTab);
-                // })
+                sale.addEventListener("click", (event) => {
+                    window.open("./transactions.html", "_self");
+                })
+                saleLogo.addEventListener("click", (event) => {
+                    window.open("./transactions.html", "_self");
+                })
+                saleDiv.addEventListener("click", (event) => {
+                    window.open("./transactions.html", "_self");
+                })
+
+
+                sellLogo.src = "../Media/Icons/sell.png";
+                saleLogo.src = "../Media/Icons/sale.png"
+                sell.innerHTML = "Sell Item";
+                sale.innerHTML = "My Sales";
+
+                sellDiv.appendChild(sell);
+                sellDiv.appendChild(sellLogo);
+                userTab.appendChild(sellDiv);
+
+                saleDiv.appendChild(sale);
+                saleDiv.appendChild(saleLogo);
+                userTab.appendChild(saleDiv);
+
+                userContainer.addEventListener("click", (event) => {
+                    const u = document.querySelector(".username")
+                    const ui = document.querySelector(".userImage")
+                    u.style.display = "none";
+                    userContainer.appendChild(userTab);
+                })
             }
-            // userTab.addEventListener("mouseleave", (event) => {
-            //     const u = document.querySelector(".username")
-            //     u.style.display = "";
-            //     userContainer.querySelector(".userTab").remove();
-            // })
+            userTab.addEventListener("mouseleave", (event) => {
+                const u = document.querySelector(".username")
+                u.style.display = "";
+                userContainer.querySelector(".userTab").remove();
+            })
         }
     }
     function closeUserTab() {
