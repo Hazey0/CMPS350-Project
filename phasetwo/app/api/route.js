b954636265e7bbe67e09a69115ebb4c8488ea76b
Hazey0 <egle.man66@gmail.com>
2024-04-29 21:52:46 +0300

    init route.js and repos

---
+++ phasetwo/app/api/route.js
@@ -0,0 +1,19 @@
+
+export async function GET() {
+
+    return new Response(await  )
+        
+    
+    }
+export async function POST() {
+
+    return new Response(await  )
+        
+    
+    }
+export async function DELETE() {
+
+    return new Response(await  )
+        
+    
+    }
