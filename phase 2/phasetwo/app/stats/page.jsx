259f72192d1fc835dfc27f66a9b06d5142fda80f
Hazey0 <egle.man66@gmail.com>
2024-05-10 18:13:30 +0300

    tiny hover style

--- phase 2/phasetwo/app/stats/page.jsx
+++ phase 2/phasetwo/app/stats/page.jsx
@@ -13,12 +13,12 @@ export default async function Home() {
       <body className="ml-5 mt-5">
         <h2> Statistics</h2>
         <div className="color grid grid-cols-4 grid-rows-8">
-          <Button className="mb-3 col-start-1 row-start-3 colored" variant="contained">Currently Listed Phones: {stats.currentListedPhone}</Button>
-          <Button className="mb-3 col-start-1 row-start-4 colored" variant="contained">Sold Phones: {stats.soldPhones}</Button>
-          <Button className="mb-3 col-start-1 row-start-5 colored" variant="contained">Number Of Sellers: {stats.numberOfSellers}</Button>
-          <Button className="mb-3 col-start-1 row-start-6 colored" variant="contained">Number Of Customers: {stats.numberOfCustomers}</Button>
-          <Button className="mb-3 col-start-1 row-start-7 colored" variant="contained">Guests Visted Today: {stats.vistedToday}</Button>
-          <Button className="mb-3 col-start-1 row-start-8 colored" variant="contained">Phones Listed Today: {stats.listedToday}</Button>
+          <Button color="secondary"  className="mb-3 col-start-1 row-start-3 colored" variant="contained">Currently Listed Phones: {stats.currentListedPhone}</Button>
+          <Button  className="mb-3 col-start-1 row-start-4 colored" variant="contained">Sold Phones: {stats.soldPhones}</Button>
+          <Button  className="mb-3 col-start-1 row-start-5 colored" variant="contained">Number Of Sellers: {stats.numberOfSellers}</Button>
+          <Button  className="mb-3 col-start-1 row-start-6 colored" variant="contained">Number Of Customers: {stats.numberOfCustomers}</Button>
+          <Button  className="mb-3 col-start-1 row-start-7 colored" variant="contained">Guests Visted Today: {stats.vistedToday}</Button>
+          <Button  className="mb-3 col-start-1 row-start-8 colored" variant="contained">Phones Listed Today: {stats.listedToday}</Button>
         </div>
       </body>
 
