576255516455ce26853f785cba02299a4be6fac7
Ahmed Faseeh <aa2005685@qu.edu.qa>
2024-05-10 13:24:09 +0300

    Stats button now links to stats react page

--- phase 2/phasetwo/app/stats/page.jsx
+++ phase 2/phasetwo/app/stats/page.jsx
@@ -4,6 +4,38 @@ export default async function Home() {
 
   
     return (
+      <>
+
+<section id="'Navigation">
+        <nav>
+            <div class="nav1">
+
+                 <div class="navItem" id="logo">
+                        <div class="logo">
+                            <a href="../Websites/main.html">
+                                <img src="../Media/Icons/phoney.png" alt="Logo" width="80" height="80" />
+                            </a>
+                        </div>
+                 </div>
+
+                 <div class="navItem">
+                    
+                    
+                 </div>
+                 <button id="statsButton" class="statsButton">
+                  <a class="statsLink" href="./stats.html">
+                      Statistics
+                  </a>
+              </button>
+
+                 <div class="user" id="user"></div>\
+                 <div class="navItem" id="loginButton">
+                    <a class="login" href="login.html" id="login">Login</a>
+                 </div>
+            </div>
+    </nav>
+    </section>
+
       <body className="ml-5 mt-5">
         <h2> Statistics</h2>
         <div className="color">
@@ -15,5 +47,6 @@ export default async function Home() {
           <p>Phones Listed Today: {stats.listedToday}</p>
         </div>
       </body>
+      </>
     );
   }
