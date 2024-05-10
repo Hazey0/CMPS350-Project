acbf6439775b9880dfd6759648a4177c2fd34e27
Ahmed Faseeh <aa2005685@qu.edu.qa>
2024-05-10 12:58:47 +0300

    Update stats.js

--- phase 2/phasetwo/public/Scripts/stats.js
+++ phase 2/phasetwo/public/Scripts/stats.js
@@ -1,8 +1,16 @@
-import { showUserTab } from "./userTabFunction.js";
-import { logged } from "./LogFunction.js";
-
-document.addEventListener("DOMContentLoaded",()=>{
-
-    logged();
-    showUserTab()
+document.addEventListener("DOMContentLoaded", async()=>{
+const stats=document.querySelector("#stats")
+//stats.appendChild(await getStats())
+getStats()
 })
+
+
+
+async function getStats(){
+    const data=[]
+    const res=await fetch("http://localhost:3000").
+    then((d)=>d.json()).then((e)=>{console.log(e);})
+  
+   
+    
+}
